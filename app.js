18ce5bdf9a135e1ea9cf4bab93dca3de52082dc0
Shaun Burdick <github@shaunburdick.com>
2014-12-12 18:06:51 -0500

    Added PTDB

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var config = require('./config'),
   VError = require('verror'),
   Command = require('./lib/command'),
-  Slack = require('node-slack'),
+  Slack = require('node-slack')
+  PTDB = require('ptdb'),
   app = require('express')(),
   app.use(require('body-parser').json());
 
